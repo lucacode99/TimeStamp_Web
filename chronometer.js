db611c787e305cc381cb44b285cbe7a8c9db1798
Lucas Pfister <digitalservices@outlook.de>
2022-09-02 16:50:05 +0200

    first commit

---
+++ chronometer.js
@@ -0,0 +1,47 @@
+class Chronometer {
+    constructor() {
+      this.currentTime = 0;
+      this.intervalId = null;
+    }
+  
+    start(callback) {
+      this.intervalID = setInterval(() =>{
+      this.currentTime++;
+      if(callback) callback();
+      }, 1000)
+      
+    }
+  
+    getMinutes() {
+      let sumOfTime = Math.floor(this.currentTime / 60)
+      return Number(sumOfTime);
+   }
+  
+    getSeconds() {
+      let remainingSeconds = this.currentTime % 60;
+      return remainingSeconds;
+  
+    }
+  
+    computeTwoDigitNumber(value) {
+      let StringOutput = value.toString();
+      
+      if (value < 10) {
+        let valueBeneathTen = value.toString();
+        return `0${valueBeneathTen}`;                          
+          } else
+          return StringOutput;
+          }
+  
+    stop() {
+      clearInterval(this.intervalID)
+    }
+  
+    reset() {
+        this.currentTime = 0;
+    }
+  
+    split() {
+     return `${this.computeTwoDigitNumber(this.getMinutes(this.currentTime))}:${this.computeTwoDigitNumber(this.getSeconds(this.currentTime))}`
+        }
+  }  
---
+++ index.js
@@ -0,0 +1,101 @@
+const chronometer = new Chronometer();
+
+// get the buttons:
+const btnLeftElement = document.getElementById('btnLeft');
+const btnRightElement = document.getElementById('btnRight');
+
+// get the DOM elements that will serve us to display the time:
+const minDecElement = document.getElementById('minDec');
+const minUniElement = document.getElementById('minUni');
+const secDecElement = document.getElementById('secDec');
+const secUniElement = document.getElementById('secUni');
+const milDecElement = document.getElementById('milDec');
+const milUniElement = document.getElementById('milUni');
+const splitsElement = document.getElementById('splits');
+
+function printTime() {
+  let actualTime = chronometer.split ()
+  minDecElement.innerText = actualTime[0] 
+  minUniElement.innerText = actualTime[1]
+  secDecElement.innerText = actualTime[3]
+  secUniElement.innerText = actualTime[4]
+  
+}
+
+function printSplit() {
+  let timeStamps = document.createElement('li');
+  splitsElement.append(timeStamps);
+  timeStamps.innerHTML = `${minDecElement.innerText}${minUniElement.innerText}:${secDecElement.innerText}${secUniElement.innerText}`
+}
+
+function clearSplits() {
+  chronometer.reset();
+  minDecElement.innerText = 0; 
+  minUniElement.innerText = 0;
+  secDecElement.innerText = 0;
+  secUniElement.innerText = 0;
+  splits.innerHTML = ' ';
+}
+
+function setStopBtn() {
+  btnLeftElement.innerText = 'STOP';
+  btnLeftElement.className = 'btn stop'
+}
+
+function setSplitBtn() {
+  btnRightElement.innerText = 'SPLIT';
+  btnRightElement.className = 'btn split'
+}
+
+function setStartBtn() {
+  btnLeftElement.innerText = 'START';
+  btnLeftElement.className = 'btn start'
+}
+
+function setResetBtn() {
+  btnRightElement.innerText = 'RESET';
+  btnRightElement.className = 'btn reset'
+}
+
+function stopMeasurement() {
+  chronometer.stop()
+}
+
+// Start/Stop Button
+btnLeftElement.addEventListener('click', () => {
+
+  
+  if (btnLeftElement.className.includes('start')) {
+    setStopBtn();
+    setSplitBtn()
+    chronometer.start(printTime)
+  } else if (btnLeftElement.className.includes('stop')) {
+    setStartBtn();
+    setResetBtn();
+    stopMeasurement()
+  }
+       
+});
+
+// Reset/Split Button
+btnRightElement.addEventListener('click', () => {
+
+  if (btnRightElement.className.includes('reset')){
+    clearSplits()
+    
+  } else if (btnRightElement.className.includes('split')){
+    printSplit()
+}
+
+});
+
+function saveFee() {
+    let fee = document.getElementById('feeId').value
+    console.log(fee)
+}
+
+
+// Eingabe des Inputs als Variable speichern
+// funktion, die "Größe" des aktuellen Splits erkennt
+// mal Stundenlohn multipliziert
+// und als text neben den Split printet
